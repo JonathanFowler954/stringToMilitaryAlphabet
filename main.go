b2b6a0aadf27b3ab54fcbd90fbd8a06df4ae7b83
jonathan.a.fowler@gmail.com <jonathan.a.fowler@gmail.com>
2024-06-20 12:51:58 -0400

    Mapped Alphabet to Military equivalent and created a main application to run library.

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ import (
 
 func main() {
 	text := "This is a test string!"
-	phoneticString := phonetic.ToPhonetic(text)
+	phoneticString := phonetic.ToMilitaryPhonetic(text)
 	fmt.Println(phoneticString) // Output: TANGO HOTEL INDIA SIERRA INDIA SIERRA ALPHA TANGO ECHO SIERRA TANGO SIERRA ROMEO INDIA NOVEMBER GOLF !
 }
