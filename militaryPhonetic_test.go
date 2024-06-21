fab5772874bee1c4134f1c885242d94fa85c4697
jonathan.a.fowler@gmail.com <jonathan.a.fowler@gmail.com>
2024-06-21 11:03:33 -0400

    Updated to use table driven testing

--- militaryPhonetic_test.go
+++ militaryPhonetic_test.go
@@ -6,24 +6,27 @@ import (
 
 // TestConvertTextToPhonetic tests the ConvertTextToPhonetic function
 func TestConvertTextToPhonetic(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		input    string
 		expected string
 	}{
-		{"A", "Alpha"},
-		{"B", "Bravo"},
-		{"C", "Charlie"},
-		{"Hello World", "Hotel Echo Lima Lima Oscar Space Whiskey Oscar Romeo Lima Delta"},
-		{"GoLang 123", "Golf Oscar Lima Alpha November Golf Space One Two Three"},
-		{"", ""},
-		{"123", "One Two Three"},
-		{"!@#", "! @ #"},
+		"Alpha":       {"A", "Alpha"},
+		"Bravo":       {"B", "Bravo"},
+		"Charlie":     {"C", "Charlie"},
+		"Hello World": {"Hello World", "Hotel Echo Lima Lima Oscar Space Whiskey Oscar Romeo Lima Delta"},
+		"GoLang 123":  {"GoLang 123", "Golf Oscar Lima Alpha November Golf Space One Two Three"},
+		"no text":     {"", ""},
+		"123":         {"123", "One Two Three"},
+		"Symbols !@#": {"!@#", "! @ #"},
 	}
 
-	for _, test := range tests {
-		result := ConvertTextToPhonetic(test.input)
-		if result != test.expected {
-			t.Errorf("ConvertTextToPhonetic(%q) = %q; want %q", test.input, result, test.expected)
-		}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ConvertTextToPhonetic(test.input)
+			if result != test.expected {
+				t.Errorf("ConvertTextToPhonetic(%q) = %q; want %q", test.input, result, test.expected)
+			}
+		})
 	}
+
 }
